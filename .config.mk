10b8a6ed4cbe6586cc192b0eaff778560dae3aa8
Philipp Paulweber <p.paulweber@gmail.com>
2016-12-20 18:03:45 +0100

    Building
    * updated make/cmake integration

--- .config.mk
+++ .config.mk
@@ -95,7 +95,7 @@ $(TESTS):%-test: %
 	@$(MAKE) $(MFLAGS) --no-print-directory \
 	-C $(OBJ) $(TARGET)-check
 	@echo "-- Running unit test"
-	@$(ENV_FLAGS) ./$(OBJ)/$(TARGET)-check
+	@$(ENV_FLAGS) ./$(OBJ)/$(TARGET)-check $(ENV_ARGS)
 
 
 benchmark: debug-benchmark
@@ -106,4 +106,4 @@ $(BENCH):%-benchmark: %
 	@$(MAKE) $(MFLAGS) --no-print-directory \
 	-C $(OBJ) $(TARGET)-run
 	@echo "-- Running benchmark"
-	@$(ENV_FLAGS) ./$(OBJ)/$(TARGET)-run
+	@$(ENV_FLAGS) ./$(OBJ)/$(TARGET)-run $(ENV_ARGS)
